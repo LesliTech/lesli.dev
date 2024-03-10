5f97053ce1a85c27a735f27d88abf30c2d0f8ef8
Luis Donis <ldonis.emc2@gmail.com>
2024-03-09 19:58:50 -0600

    deploy Sat Mar  9 19:58:01 CST 2024

---
+++ assets/index.md.nvkjMvph.js
@@ -0,0 +1,118 @@
+import{o as t,c as s,k as e,F as r,E as d,n as _,a as g,t as o,m as a,I as c,U as b,ab as x}from"./chunks/framework.V7QYn7G9.js";import{_ as v}from"./chunks/lesli-engines.TYfCW54X.js";import{_ as y}from"./chunks/lesli-footer.vbPM7xi9.js";import"./chunks/engine-babel.vy9msxKc.js";const w={class:"lesli-component-links lesli-page-content-boxes"},$={class:"container"},k={class:"columns"},S={class:"column"},A=["href"],L={__name:"lesli-links",setup(u){/*
+Lesli
+
+Copyright (c) 2023, Lesli Technologies, S. A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+Lesli · Ruby on Rails SaaS Development Framework.
+
+Made with ♥ by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@contact  hello@lesli.tech
+@website  https://www.lesli.tech
+@license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/const l=[{href:"/about/",icon:"ri-heart-line",text:"About"},{href:"/start/",icon:"ri-send-plane-line",text:"Getting started"},{href:"/contrib/",icon:"ri-git-pull-request-line",text:"Contributing"},{href:"/docs.html",icon:"ri-book-2-line",text:"Documentation"}];return(m,n)=>(t(),s("section",w,[e("div",$,[e("div",k,[(t(),s(r,null,d(l,i=>e("div",S,[e("a",{class:"has-background-white",href:i.href},[e("i",{class:_(i.icon)},null,2),g(" "+o(i.text),1)],8,A)])),64))])])]))}},T="<b>Lesli</b> was designed to create highly customizable, scalable and secure software products.",R="Scale your business, <i>with Us</i> and Our tools",C=[{icon:"ri-database-line",name:"Database",text:"Scalable, extensible and easy to use PostgreSQL database",items:["Relational design","Versionable database","Easy to scale","Maintenable"]},{icon:"ri-server-line",name:"Backend",text:"Built-on top of Ruby and Ruby on Rails",items:["Fully extendable","Modular architecture","Roles & privileges ready","Activity tracking systems"]},{icon:"ri-window-line",name:"Frontend",text:"Multichannel & multiple client apps",items:["Web application","Desktop client","Android app","iOS app","Rest API"]}],f={description:T,link:R,components:C},D={class:"lesli-component-description has-background-white"},M={class:"component-header"},F=["innerHTML"],V={class:"columns is-flex-wrap-wrap"},B={class:"column"},P={__name:"lesli-description",setup(u){/*
+Lesli
+
+Copyright (c) 2023, Lesli Technologies, S. A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+Lesli · Ruby on Rails SaaS Development Framework.
+
+Made with ♥ by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@contact  hello@lesli.tech
+@website  https://www.lesli.tech
+@license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/return(l,m)=>(t(),s("section",D,[e("div",M,[e("p",{innerHTML:a(f).description},null,8,F)]),e("div",V,[(t(!0),s(r,null,d(a(f).components,n=>(t(),s("div",B,[e("div",null,[e("i",{class:_(n.icon)},null,2),e("h3",null,o(n.name),1),e("p",null,o(n.text),1),e("ul",null,[(t(!0),s(r,null,d(n.items,i=>(t(),s("li",null,o(i),1))),256))])])]))),256))])]))}},N={class:"lesli-techstack"},O=e("li",{class:"is-hidden-touch"},[e("p",null,"built with awesome technologies")],-1),q=["alt","src"],E={__name:"lesli-techstack",setup(u){/*
+
+Lesli
+
+Copyright (c) 2023, Lesli Technologies, S. A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+Lesli · Ruby on Rails SaaS Development Framework.
+
+Made with ♥ by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@contact  hello@lesli.tech
+@website  https://www.lesli.tech
+@license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/const l=["postgresql","rails","vue","sass","bulma"];return(m,n)=>(t(),s("section",N,[e("ul",null,[O,(t(),s(r,null,d(l,i=>e("li",null,[e("img",{alt:i,src:`/images/tools/logo-${i}.svg`},null,8,q)])),64))])]))}},I="Batteries are included",U="Lesli comes with the features your software needs",W=[{icon:"ri-palette-line",name:"Theming",text:"Make staff feel at home by theming the Web App, Desktop Clients and Mobile Apps."},{icon:"ri-search-line",name:"Full text search",text:"Enable users to find their data quick and easy through global or local search."},{icon:"ri-link",name:"Public Link Sharing",text:"Share files and folders with outside contacts, protected by passwords and expiry dates."},{icon:"ri-file-shield-2-line",name:"Secure View",text:"Let users safely distribute confidential documents with maximum control."},{icon:"ri-terminal-box-line",name:"Commands",text:"A better way to interact with Lesli via chatbot, voice assistant or typing commands."},{icon:"ri-shield-user-line",name:"Advance role management",text:"Add two or more factors for login, such as passwords, one-time codes or magic links."},{icon:"ri-shield-keyhole-line",name:"Detailed privileges for users",text:"Add two or more factors for login, such as passwords, one-time codes or magic links."},{icon:"ri-passport-line",name:"Multi-Factor Authentication",text:"Add two or more factors for login, such as passwords, one-time codes or magic links."},{icon:"ri-file-paper-line",name:"System Auditing",text:"Log actions of users and admins for compliance and security."},{icon:"ri-global-line",name:"Multilanguage",text:"Available in any language needed."}],p={title:I,text:U,content:W},z={class:"lesli-component-core-features hero is-fullheight"},G={class:"hero-body"},H={class:"container"},J={class:"component-header"},j={class:"columns"},Q={class:"column"},K={class:"feature"},X={__name:"lesli-corefeatures",setup(u){/*
+Lesli
+
+Copyright (c) 2023, Lesli Technologies, S. A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+Lesli · Ruby on Rails SaaS Development Framework.
+
+Made with ♥ by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@contact  hello@lesli.tech
+@website  https://www.lesli.tech
+@license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/return(l,m)=>(t(),s("section",z,[e("div",G,[e("div",H,[e("div",J,[e("h2",null,o(a(p).title),1),e("p",null,o(a(p).text),1)]),e("div",j,[(t(!0),s(r,null,d(a(p).content,n=>(t(),s("div",Q,[e("div",K,[e("figure",null,[e("i",{class:_(n.icon)},null,2)]),e("h3",null,o(n.name),1),e("p",null,o(n.text),1)])]))),256))])])])]))}},Y="By developers for developers",Z="Lesli is built and maintained according to high-quality software development standards.",ee=[{icon:"ri-code-line",name:"Code Generation",text:"Rails scaffolds with vitamins."},{icon:"ri-screenshot-fill",name:"UI Templates",text:"Just copy and paste the examples to quickly build a beautiful app."},{icon:"ri-smartphone-line",name:"Responsive Design",text:"Our interfaces work perfectly on all devices."},{icon:"ri-code-box-line",name:"Complete Test Coverage",text:"We implement a full suite of automated integration tests so there are no bugs in your software."},{icon:"ri-refresh-line",name:"Software up to date",text:"We work with the latest versions of the tools."}],h={title:Y,text:Z,content:ee},te={class:"lesli-component-development"},se={class:"container"},ne={class:"component-header"},oe={class:"hero-body"},ie={class:"columns"},ae={class:"column"},le={class:"feature has-background-white"},ce={__name:"lesli-development",setup(u){return(l,m)=>(t(),s("section",te,[e("div",se,[e("div",ne,[e("h2",null,o(a(h).title),1),e("p",null,o(a(h).text),1)]),e("div",oe,[e("div",ie,[(t(!0),s(r,null,d(a(h).content,n=>(t(),s("div",ae,[e("div",le,[e("figure",null,[e("i",{class:_(n.icon)},null,2)]),e("h3",null,o(n.name),1),e("p",null,o(n.text),1)])]))),256))])])])]))}},re={class:"hero is-medium lesli-page-header"},de=b('<section class="hero-body"><div class="container"><img class="mb-5 logo" alt="Lesli Framework logo" src="'+x+'"><h1 class="title"> The Open Source Ruby on Rails <span class="lesli-title-colored">SaaS Development Framework.</span></h1><p class="description"> Lesli provides all the necessary tools to build your SaaS, empowering you and your team to focus on business and what makes your software special. </p></div></section>',1),fe=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","pageClass":"lesli-page"},"headers":[],"relativePath":"index.md","filePath":"index.md","lastUpdated":1709692281000}'),ue={name:"index.md"},ge=Object.assign(ue,{setup(u){return(l,m)=>(t(),s("div",null,[e("header",re,[de,c(L,{class:"lesli-component-links"})]),c(E),c(P),c(X),c(v),c(ce),c(y)]))}});export{fe as __pageData,ge as default};
---
+++ assets/vue_elements_link.md.nLrU8UBv.lean.js
@@ -0,0 +1,31 @@
+import{a2 as c,D as d,o as s,b as i,w as a,J as h,ae as u,af as k,r as m,c as l,I as o,a as _,U as g,m as E}from"./chunks/framework.V7QYn7G9.js";const f={__name:"Link",props:{to:{type:String,required:!0},button:{type:Boolean,default:!1},icon:{},loading:{},solid:{},info:{},success:{},warning:{},danger:{},small:{}},emits:["click"],setup(t,{emit:n}){/*
+
+Lesli
+
+Copyright (c) 2023, Lesli Technologies, S. A.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+Lesli · Ruby on Rails SaaS Development Framework.
+
+Made with ♥ by https://www.lesli.tech
+Building a better future, one line of code at a time.
+
+@contact  hello@lesli.tech
+@website  https://www.lesli.tech
+@license  GPLv3 http://www.gnu.org/licenses/gpl-3.0.en.html
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+*/c();const e=t;return(r,P)=>{const p=d("router-link");return s(),i(p,{to:e.to.toString()},{default:a(()=>[(s(),i(h(e.button?"lesli-button":"span"),u(k(e)),{default:a(()=>[m(r.$slots,"default")]),_:3},16))]),_:3},8,["to"])}}},v=g("",8);JSON.parse('{"title":"Links","description":"","frontmatter":{},"headers":[],"relativePath":"../../lesli-vue/source/elements/link/Link.md","filePath":"../../lesli-vue/source/elements/link/Link.md","lastUpdated":null}');const b={name:"../../lesli-vue/source/elements/link/Link.md"},y=Object.assign(b,{setup(t){return(n,e)=>(s(),l("div",null,[v,o(f,{to:"holi"},{default:a(()=>[_(" My link ")]),_:1})]))}}),S=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"vue/elements/link.md","filePath":"vue/elements/link.md","lastUpdated":1709692281000}'),x={name:"vue/elements/link.md"},q=Object.assign(x,{setup(t){return(n,e)=>(s(),l("div",null,[o(E(y))]))}});export{S as __pageData,q as default};
